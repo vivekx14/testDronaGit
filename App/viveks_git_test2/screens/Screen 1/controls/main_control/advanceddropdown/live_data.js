cdaf53e8d3c53eca9ec282255b6e0f31891847e8
brijesha <brijesh@studio.com>
2025-01-27 18:09:05 +0530

    'viveks_git_test2' App added to git App configuration added. JSOBJECTS added. Dependency added App icon updated. 'Screen 1' screen added.

---
+++ App/viveks_git_test2/screens/Screen 1/controls/main_control/advanceddropdown/live_data.js
@@ -0,0 +1,26 @@
+[
+    {
+        "value": "approve",
+        "name": "Approve",
+        "description": "John@studio.com",
+        "color": "#26bc6a",
+        "image": "https://dronamobilepublic.s3.amazonaws.com/DRONA5_Team108/content/app/images/public/Tick_c5Abe.svg",
+        "_isSelected": true
+    },
+    {
+        "value": "reject",
+        "name": "Reject",
+        "description": "Dave@studio.com",
+        "color": "#FF5656",
+        "image": "https://dronamobilepublic.s3.amazonaws.com/DRONA5_Team108/content/app/images/public/Close_L8P9q.svg",
+        "_isSelected": false
+    },
+    {
+        "value": "none",
+        "name": "None",
+        "description": "Doe@studio.com",
+        "color": "#F5AB00",
+        "image": "https://dronamobilepublic.s3.amazonaws.com/DRONA5_Team108/content/app/images/public/Info_BCFfd.svg",
+        "_isSelected": false
+    }
+]
