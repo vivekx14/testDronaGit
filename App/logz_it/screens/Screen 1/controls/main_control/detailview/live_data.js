b1554cec7aa6bb2810ac670bf97f68601c7848b9
brijesha <brijesh@studio.com>
2025-01-29 12:09:05 +0530

    'logz_it' App added to git App configuration added. JSOBJECTS added. Dependency added App icon updated. 'Screen 1' screen added.

---
+++ App/logz_it/screens/Screen 1/controls/main_control/detailview/live_data.js
@@ -0,0 +1,10 @@
+[
+    {
+        "fullname": "Maria Gerhold",
+        "email": "maria81@example.com",
+        "address": "57177 Kuhlman Shoal",
+        "birthdate": 89596800000,
+        "phone": 3241244324,
+        "avatar": "https://robohash.org/etitaquevoluptate.png?size=300x300&set=set1"
+    }
+]
