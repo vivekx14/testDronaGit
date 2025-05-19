6b989727968a48c83db64d513e21bdc7d3d90b91
brijesha <brijesh@studio.com>
2025-05-19 18:25:41 +0530

    vks_git:: Minor app update

---
+++ App/vks_git/screens/Screen 1/controls/main_control/advanceddropdown/live_data.js
@@ -0,0 +1,26 @@
+[
+    {
+        "value": "approve",
+        "name": "",
+        "description": "John@studio.com",
+        "color": "#26bc6a",
+        "image": "https://dronamobilepublic.s3.amazonaws.com/DRONA5_Team108/content/app/images/public/Tick_c5Abe.svg",
+        "_isSelected": true
+    },
+    {
+        "value": "reject",
+        "name": "",
+        "description": "Dave@studio.com",
+        "color": "#FF5656",
+        "image": "https://dronamobilepublic.s3.amazonaws.com/DRONA5_Team108/content/app/images/public/Close_L8P9q.svg",
+        "_isSelected": false
+    },
+    {
+        "value": "none",
+        "name": "",
+        "description": "Doe@studio.com",
+        "color": "#F5AB00",
+        "image": "https://dronamobilepublic.s3.amazonaws.com/DRONA5_Team108/content/app/images/public/Info_BCFfd.svg",
+        "_isSelected": false
+    }
+]
